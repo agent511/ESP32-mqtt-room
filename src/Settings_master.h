9030635ba388df0833c29f3bfddd20bce2a9d294
Mark Hartman <agent511@users.noreply.github.com>
2021-07-20 15:11:21 -0700

    Add room settings files

---
+++ src/Settings_master.h
@@ -0,0 +1,8 @@
+//Replace with a human-friendly host name. Must not contain spaces or special characters and be unique on your network
+#define hostname "esp32_master"
+
+//Replace with the room name where the node will be placed; example: #define room "living-room"
+#define room "master"
+
+// Maximum distance (in meters) to report. Devices that are calculated to be further than this distance in meters will not be reported
+#define maxDistance 4
